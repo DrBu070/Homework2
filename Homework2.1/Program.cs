b8e3ad697fc81157b38c8a2ed5c7063ca2779b09
IvanovVladislav <ivanov2703@mail.ru>
2022-07-16 21:48:53 +0300

    add Homework2.1

---
+++ Homework2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Задача 10");
+Console.WriteLine("..........................................................");
+
+int number, number2;
+
+number = new Random().Next(100, 900);
+Console.WriteLine("Исходное число: " + number);
+
+number2 = (number/10) % 10;
+Console.WriteLine("Итоговое число: " + number2);
+
